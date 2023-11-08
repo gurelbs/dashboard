39273d6d689ba3396c7e904c0115faa5930491e1
f8ka <freegazachildren@proton.me>
2023-11-08 11:52:58 +0200

    create scraper index file

---
+++ src/scraper/index.ts
@@ -0,0 +1,4 @@
+export { scraperConfig } from './scraper.config';
+export { ScraperConfig, FibiEnv } from './scraper.interface';
+export { ScraperService } from './scraper.service';
+export { ScraperModule } from './scraper.module';
